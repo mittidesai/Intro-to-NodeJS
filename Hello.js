55662204a71ff069f3108e3c12259de3b8fb4ec4
Miti Desai <mittidesai@gmail.com>
2019-04-07 19:23:23 -0500

    After quality check of hello.js

--- Hello.js
+++ Hello.js
@@ -2,20 +2,24 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 const PORT = process.env.PORT || 5000
-http.createServer( function(req, res) {
-	var q = url.parse(req.url,true);
-	console.log(q.pathname);
-	var filename = '.'+ q.pathname;
+
+http.createServer(function(req, res) {
+	var q = url.parse(req.url, true);
+	// console.log(q.pathname);
+	var filename = "."+ q.pathname;
+	if(filename == './') {filename = './index.html';}
+	
+
 	console.log(filename);
 	fs.readFile(filename,function(err,data){
 		if(err){
 			res.writeHead(404,{'Content-Type':'text/html'});
-			return res.end("404 Page not found");
+			return res.end("404 Page Not found");
 		}
-	res.writeHead(200,{'Content-Type':'text/html'});
-	res.write(data);
+		res.writeHead(200,{'Content-Type':'text/html'});
+		res.write(data);
 	// console.log('...Incoming request:'+' '+req.url);
-	return res.end();	
+		return res.end();	
 	});
 
 	
