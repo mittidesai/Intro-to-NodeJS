ed80a2b2faceb543a026f6804ec86856739d15aa
Miti Desai <mittidesai@gmail.com>
2019-04-13 11:13:57 -0500

    Commit

--- Hello.js
+++ Hello.js
@@ -14,7 +14,7 @@ http.createServer(function(req, res) {
 	fs.readFile(filename,function(err,data){
 		if(err){
 			res.writeHead(404,{'Content-Type':'text/html'});
-			return res.end("404 Page Not found!");
+			return res.end("404 Page Not found!!");
 		}
 		res.writeHead(200,{'Content-Type':'text/html'});
 		res.write(data);
